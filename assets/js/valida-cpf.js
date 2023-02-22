e6c1727a5749eebe2a90626f492d53e6f8a21839
btcorso <uniebcorso@fei.edu.br>
2023-02-22 10:37:00 -0300

    implementado regra cpf

--- assets/js/valida-cpf.js
+++ assets/js/valida-cpf.js
@@ -1,5 +1,66 @@
 
 export default function validaCPF(input) {
     const cpf = input.value.replace(/\.|-/g, "");
-    console.log(cpf);
+    var msg = "";
+    if (numerosRepetidos(cpf) || validaDigito1(cpf) || validaDigito2(cpf)) {
+        msg = "CPF inválido!"
+    }
+    else {
+        msg = "CPF válido!"
+    }
+
+    return console.log(msg);
+}
+
+function numerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numerosRepetidos.includes(cpf)
+}
+
+function validaDigito1(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaDigito2(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
